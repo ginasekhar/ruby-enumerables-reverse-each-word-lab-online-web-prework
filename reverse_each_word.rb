e2413ca667afb0f3be25868d3e8f42d4b37ddbe1
Gina Sekhar <ginasekhar@yahoo.com>
2019-09-23 17:07:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,6 @@
 def reverse_each_word(input_string)
   input_array = input_string.split(" ")
   
-  reverse_array  = input_array.collect{
-  i = 0
-  input_array.each do |each_word|
-    reverse_array[i] = each_word.reverse
-    i = i+1
+  reverse_array  = input_array.collect{ |each_word| each_word.reverse}
+  
   end
-  reverse_array
-  reverse_string = reverse_array.to_str
-  reverse_string
-end
